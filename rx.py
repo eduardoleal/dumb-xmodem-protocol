1eec7a8cf1992bfae781c71d555697cc8b395047
Eduardo Leal <me@lealeduardo.com>
2015-03-28 09:45:51 -0300

    lock

--- rx.py
+++ rx.py
@@ -6,41 +6,32 @@ s = serial.Serial('/dev/pts/27')
 
 # check  packages
 def check(seq, seqn, mensagem, somarec):
-    somaesp = 0
-
-    if seq - seqn == 0:
-        somaesp = int(mensagem) + somaesp % 256
-
+    if (seq + int(seqn) == 0):
+        fcs = 0
         for i in range(128):
-            somaesp = somaesp + int (mensagem[i])
-
-        somaesp = somaesp % 256
-
-        if somaesp == somarec:
-            return True
-
-        return False
-
-    return False
-
+            fcs = fcs + ord(mensagem[i])
+        fcs = fcs % 256
+        if (fcs == somarec):
+            return 1
+    return 0
 
 soh = s.read(1)
-if soh == str (0x01):
-    seq = int(s.read())
-    # print seq
-    seqn = s.read(1)
-    # print seqn
+print str(0x01)
+if soh == str(0x01):
+    seq = int(s.read(1))
+    print seq
+    seqn = s.read(2)
+    print seqn
     mensagem = s.read(128)
     print mensagem
     somarec = s.read(1)
-    # print somarec
+    print somarec
 
-    if check(seq, seq, mensagem, somarec):
+    if check(seq, seqn, mensagem, somarec) == 1:
         s.write(str(0x06))
     else:
         s.write(str(0x15))
 
-else:
-    if soh == str (0x04):
-        s.write(str(0x06))
-        s.close
+
+if soh == str (0x04):
+    s.close
--- tx.py
+++ tx.py
@@ -1,71 +1,34 @@
-#codigo do transmissor - XMODEM
-
 import serial
-s = serial.Serial('/dev/pts/1')
+s = serial.Serial('/dev/pts/7')
 
-seq = 0x01
-data = "ABC"
-
-#
-# qnd de pacotes
-#
-def qtdpac(x):
-    size = len(x) / 128
-    if (len(x) % 128 > 0):
-        size =+ 1
-    return size
-
-#
-# envia pedacos pacote
-#
-def txp (seq, data):
-
-    fim = False
-    while (fim == False):
-        fcs = 0
+def txp(seq, data):
+    end = false
+    while (-end):
         soh = str(0x01)
         s.write(soh)
-        print soh
         s.write(str(seq))
-        print seq
-        inverso = -seq
-        s.write (str(inverso))
-        print (inverso)
+        s.write(str(-seq))
 
-        for i in range(len(data)):
-            fcs = fcs + 1
+        fcs = 0
+        for i in range(128):
+            fcs =+ int(data[i])
 
-
-        for i in range(128 - len(data)):
-            data = data + ' '
-
-        data2 = 0
-
-        for c in range(len(data)):
-            data2 = data2 + c
-
-
-        s.write(str(data2))
-        print data2
+        fcs = fcs % 256
+        s.write(data)
         s.write(str(fcs))
-        print (fcs)
-        rx = s.read()
-        if (rx == str(0x06)):
-            fim = True
+        rx = s.read(1)
+        if(rx == str(0x06)):
+            end = true
 
-
-#
-# Main
-#
-qtd = qtdpac(data)
-while (qtd > 0):
-    txp(seq, data)
-    seq = seq + 0x01
-    qtd = qtd - 1
-
-
-#
-#  termina envio
-#
-# s.write(str (0x04))
-# s.close
+def txd(data):
+    n = len(data) / 128
+    if (len(data) % 128 > 0):
+        n =+ 1
+    for i in range(n):
+        seq = i % 256 + 1
+        j = i * 128
+        dp = data[j:j+128]
+        txp(seq, dp)
+info = 'abc'
+txd(data)
+s.close()
