e30bd98abc155b986be091b5fa818ee33e469063
Eduardo Leal <me@lealeduardo.com>
2015-03-28 09:47:47 -0300

    fuck

--- tx.py
+++ tx.py
@@ -2,8 +2,8 @@ import serial
 s = serial.Serial('/dev/pts/7')
 
 def txp(seq, data):
-    end = false
-    while (-end):
+    end = 0
+    while (end == 0):
         soh = str(0x01)
         s.write(soh)
         s.write(str(seq))
@@ -11,24 +11,24 @@ def txp(seq, data):
 
         fcs = 0
         for i in range(128):
-            fcs =+ int(data[i])
+            fcs = fcs + int(data[i])
 
         fcs = fcs % 256
         s.write(data)
         s.write(str(fcs))
         rx = s.read(1)
         if(rx == str(0x06)):
-            end = true
+            end = 1
 
 def txd(data):
     n = len(data) / 128
     if (len(data) % 128 > 0):
-        n =+ 1
+        n = n + 1
     for i in range(n):
         seq = i % 256 + 1
         j = i * 128
         dp = data[j:j+128]
         txp(seq, dp)
 info = 'abc'
-txd(data)
+txd(info)
 s.close()
